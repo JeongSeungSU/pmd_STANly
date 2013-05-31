7492b4570dae553cad0d13b8cfd3a8e977634a9b
sjdmldi <sjdmldi@gmail.com>
2013-05-31 17:57:35 +0900

    Violation 객체 getter setter 추가

--- src/main/java/net/sourceforge/pmd/lang/java/rule/Violation.java
+++ src/main/java/net/sourceforge/pmd/lang/java/rule/Violation.java
@@ -4,6 +4,31 @@ public class Violation {
 	public final static int BASIC = 0;
 	public final static int NAMING = 1;
 	private Integer violationType;//0: Basic, 1:Naming
+	
+	public Integer getViolationType() {
+		return violationType;
+	}
+	public void setViolationType(Integer violationType) {
+		this.violationType = violationType;
+	}
+	public String getSourcePath() {
+		return sourcePath;
+	}
+	public void setSourcePath(String sourcePath) {
+		this.sourcePath = sourcePath;
+	}
+	public int getSourceLine() {
+		return sourceLine;
+	}
+	public void setSourceLine(int sourceLine) {
+		this.sourceLine = sourceLine;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
 	private String sourcePath;
 	private int sourceLine;
 	private String message;
